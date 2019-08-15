09079383c2d575fd8a8a02855351316c4a3a4812
manjitwaikhom <manjitsinghwk@gmail.com>
2019-08-15 20:09:04 +0530

    Initial commit

---
+++ src/main/java/com/ge/healthmonitor/GeHealthMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.ge.healthmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeHealthMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeHealthMonitorApplication.class, args);
+	}
+
+}
